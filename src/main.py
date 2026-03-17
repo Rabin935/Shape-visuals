54fc08a5186157cd5d8f2b2f5d78ce28dc8da9c5
Rabin Tamang <lrabin935@gmail.com>
2026-03-17 23:06:55 +0545

    feat: implement 2D renderer with gesture-controlled shapes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import cv2
 from hand_tracking.hand_tracker import HandTracker
 from hand_tracking.gesture_detector import GestureDetector
+from visuals.renderer_2d import Renderer2D
 
 cap = cv2.VideoCapture(0)
 tracker = HandTracker()
 gesture_detector = GestureDetector()
+
+
+renderer = Renderer2D()
+position = (300, 300)
+
+
+
+
 
 while True:
     ret, frame = cap.read()
@@ -13,8 +22,14 @@ while True:
     frame, hands = tracker.process(frame)
 
     for hand in hands:
-        gesture = gesture_detector.detect_gesture(hand["landmarks"])
-        print(hand["label"], gesture)
+        if hand["label"] == "Left":
+            lm = hand["landmarks"][8]
+            position = (int(lm[0] * frame.shape[1]), int(lm[1] * frame.shape[0]))
+        else:
+            gesture = gesture_detector.detect_gesture(hand["landmarks"])
+            renderer.update(gesture)
+
+    frame = renderer.draw(frame, position)
 
     cv2.imshow("Gesture Visualizer", frame)
 
---
+++ src/visuals/renderer_2d.py
@@ -0,0 +1,32 @@
+import cv2
+
+class Renderer2D:
+    def __init__(self):
+        self.shape = "circle"
+        self.color = (0, 255, 0)
+        self.size = 30
+
+    def update(self, gesture):
+        if gesture == "fist":
+            self.shape = "square"
+        elif gesture == "peace":
+            self.shape = "circle"
+        elif gesture == "open":
+            self.color = (255, 0, 0)
+
+    def draw(self, frame, position):
+        x, y = position
+
+        if self.shape == "circle":
+            cv2.circle(frame, (x, y), self.size, self.color, -1)
+
+        elif self.shape == "square":
+            cv2.rectangle(
+                frame,
+                (x - self.size, y - self.size),
+                (x + self.size, y + self.size),
+                self.color,
+                -1
+            )
+
+        return frame
